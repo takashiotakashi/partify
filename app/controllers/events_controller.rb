bdf8ce04d8a3a524d7d9f17f4e6acbdf786f43f3
Ronaldo Peixoto <ronaldopeixotojr@gmail.com>
2022-08-29 18:35:55 -0300

    Adiciona os métodos em eventos

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,23 @@
 class EventsController < ApplicationController
-  def resources
+  def index
+    @events = Event.all
+  end
+
+  def show
+    set_event
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  private
+
+  def set_place
+    @event = Event.find(params[:id])
+  end
+
+  def place_params
+    params.require(:events).permit(:name, :address, :description, :price)
   end
 end
-
-# adicionar name address
-# colocar find para geocoding
