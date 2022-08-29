43a3d333dee42aa7b210810452987505bb8605d3
Mars Pinho <marciopinho@gmail.com>
2022-08-29 19:06:54 -0300

    added route for omniauth callbacks controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'events/resources'
   get 'resources/events'
-  devise_for :users
+  devise_for :users,
+             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   root to: "pages#home"
   resources :places, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
