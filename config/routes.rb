c9b631ff8a3e9e6f173695095e3fca92dd5c93d8
Carlos Esbeltte <carlosmechelli@gmail.com>
2022-08-29 17:55:52 -0300

    Conflicts with ronaldo done in my vscode - 29agos18h

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :places, only: %i[index show]
+  resources :genres do
+    resources :places, only: %i[index show]
+  end
 
-  resources :genres
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
