3bc3bbfd6d4fa9dc03e9eb664d9fb8d429d6ea69
Carlos Eduardo Esbeltte <104123000+takashiotakashi@users.noreply.github.com>
2022-08-30 13:40:34 -0300

    Update events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,19 +11,6 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
-  def scrape
-    busca = "rock"
-    url = "https://www.sympla.com.br/eventos/sao-paulo-sp?s=#{busca}&tab=eventos"
-
-    html_file = URI.open(url).read
-    html_doc = Nokogiri::HTML(html_file)
-
-    html_doc.search(".CustomGridstyle__CustomGridCardType").each do |element|
-      puts element.text.strip
-      puts element.attribute("href").value
-    end
-  end
-
   private
 
   def set_place
