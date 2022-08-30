50649fc30f5ce5012037ed91d8774bfe9a3c3e64
Carlos Esbeltte <carlosmechelli@gmail.com>
2022-08-30 14:43:47 -0300

    30agos14:45

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_200759) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_143949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,8 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_200759) do
     t.bigint "genre_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
     t.date "date"
     t.time "time"
     t.float "latitude"
