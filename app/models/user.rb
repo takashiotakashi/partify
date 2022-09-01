7e99aaf6d4cc9e9048ffe1bdf01e6b20cb0b73ba
Ronaldo Peixoto <ronaldopeixotojr@gmail.com>
2022-09-01 15:50:52 -0300

    Alterações para conseguir acessar spotify

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,13 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   belongs_to :fav_genre, class_name: 'Genre', optional: true
 
-  # validates :address, presence: true, on: :update Por que obrigar a colocar endereço?
+  #validates :address, presence: true, on: :update # Por que obrigar a colocar endereço?
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:spotify]
 
-  #after_create :grab_genres
+  after_create :grab_genres
 
   # has_one_attached :photo
   # has_one :favorites, dependent: :destroy # caso implantemos favorites
@@ -49,25 +49,24 @@ class User < ApplicationRecord
     return user
   end
 
-  #private
+  private
 
-#def grab_genres
+  def grab_genres
 
-##url = "https://api.spotify.com/v1/me/top/artists"
-##user_serialized = URI.open(url, "Authorization" => "Bearer #{self.token}", "Content-Type" => "application/json").read
-##parsed = JSON.parse(user_serialized)
+    url = "https://api.spotify.com/v1/me/top/artists"
+    user_serialized = URI.open(url, "Authorization" => "Bearer #{self.token}", "Content-Type" => "application/json").read
+    parsed = JSON.parse(user_serialized)
+    spotify_genres = []
+    parsed["items"].each do |artist|
+      artist["genres"].each do |genre|
+        spotify_genres << genre.split # considerar manter nomes compostos, se criarmos filtro adequado para escopo etc
+      end
+    end
+    top_genre = spotify_genres.flatten.tally.max_by { |_k, v| v }.first
 
-##spotify_genres = []
-##parsed["items"].each do |artist|
-###artist["genres"].each do |genre|
-#spotify_genres << genre.split # considerar manter nomes compostos, se criarmos filtro adequado para escopo etc
-#end
-#end
-#top_genre = spotify_genres.flatten.tally.max_by { |_k, v| v }.first
+    genre = Genre.find_by(name: top_genre)
 
-#genre = Genre.find_by(name: top_genre)
-
-#self.fav_genre = genre if genre
-#self.save!
-#end
+    self.fav_genre = genre if genre
+    self.save!
+  end
 end
