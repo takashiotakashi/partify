25850e63ab7fdb3df7e6cbf32ad7b78e94da0285
Mars Pinho <marciopinho@gmail.com>
2022-09-06 16:32:04 -0300

    algumas migrations apagadas

--- db/migrate/20220906172839_remove_genre_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveGenreFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    # remove_reference :users, :genre, null: false, index: true
-    remove_index :users, name: :index_users_on_fav_genre_id, unique: true
-  end
-end
