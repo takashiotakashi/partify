277ede68063f152738b8c045f20700c3256a1de8
Ronaldo Peixoto <ronaldopeixotojr@gmail.com>
2022-09-06 15:14:23 -0300

    Edicao de seeds metal

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,9 +4,9 @@ class EventsController < ApplicationController
     @events = Event.where(genre_id: current_user.fav_genre_id)
     # @events = Event.where(genres: current_user.fav_genre_id) implementar lógica para que match se genres do events tiver matches com o genres do user
 
-    @events = @events.near([current_user.latitude, current_user.longitude], 150) if params[:address] == "user"
+    @events = @events.near([current_user.latitude, current_user.longitude], 10) if params[:address] == "user"
 
-    @events = @events.near([params[:lat], params[:lng]], 15) if params[:lat].present? && params[:lng].present?
+    @events = @events.near([params[:lat], params[:lng]], 10) if params[:lat].present? && params[:lng].present?
 
     if @events.present?
       @markers = @events.geocoded.map do |event|
