fcb279b0304b7bcf26dad003ffffe9b9c2ad6135
Ronaldo Peixoto <ronaldopeixotojr@gmail.com>
2022-09-06 11:26:06 -0300

    Inclui pop-up dos marcadores do mapa

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -22,17 +22,31 @@ export default class extends Controller {
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
 
-        new mapboxgl.Marker()
-          .setLngLat([ marker.lng, marker.lat ])
-          .addTo(this.map)
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(this.map)
+
+      this.userValue.forEach((marker) => {
+
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+
+      new mapboxgl.Marker({
+          "color": "#EB4034"
+        })
+
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
+      .addTo(this.map)
       })
-  }
+
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-  }
-
-
+    }
+  })
 }
