74f0374904a7d53ce207605792a0964820988b73
Carlos Esbeltte <carlosmechelli@gmail.com>
2022-09-07 16:50:23 -0300

    Final stimulus favorite feature

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,17 @@
 class FavoritesController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def create
     @event = Event.find(params[:event_id])
-    return if current_user.favorites.find_by(id: @event.id)
-    
-    favorite = Favorite.new
-    favorite.event = @event
-    favorite.user = current_user
-    favorite.save
-    redirect_to @event, status: :see_other
-  end
-
-  def destroy
-    @favorite = Favorite.find(params[:id])
-    @favorite.destroy
-    redirect_to event_path(@favorite.event), status: :see_other
+    @user = User.find(params[:user_id])
+    favorite = @user.favorites.find_by(event_id: @event.id)
+    if favorite
+      favorite.destroy
+    else
+      favorite = Favorite.new
+      favorite.event = @event
+      favorite.user = @user
+      favorite.save
+    end
   end
 end
